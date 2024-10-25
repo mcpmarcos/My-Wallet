4b4d549069c4840fc9459d2004b39f5ff78a1cda
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-10-24 22:29:34 -0300

    Starting the back end.

---
+++ src/test/java/com/mcp/my_wallet/MyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.my_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
