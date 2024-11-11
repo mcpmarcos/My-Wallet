2bf7d7279d8ae7f1e7dfa993438b3fdc5164ffe2
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-11-11 20:54:06 -0300

    Adding atributes

--- src/main/java/com/mcp/my_wallet/model/Address.java
+++ src/main/java/com/mcp/my_wallet/model/Address.java
@@ -1,5 +1,40 @@
 package com.mcp.my_wallet.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Address {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String street;
+
+    private int number;
+
+    private String cep;
+
+    private String city;
+
+    private String state;
+
+    public Address(Long id, String street, int number, String cep, String city, String state) {
+        this.id = id;
+        this.street = street;
+        this.number = number;
+        this.cep = cep;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Address() {
+    
+    }
+
 }
--- src/main/java/com/mcp/my_wallet/model/Client.java
+++ src/main/java/com/mcp/my_wallet/model/Client.java
@@ -1,5 +1,37 @@
 package com.mcp.my_wallet.model;
 
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Client {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int cpf;
+
+    private String name;
+
+    private LocalDateTime date; 
+
+    public Client (Long id, int cpf, String name, LocalDateTime date) {
+        this.id = id;
+        this.cpf = cpf;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Client (){
+
+    }
+
 }
