70824e83f697a72f5ceb2c81b14fa677cdfcaa0c
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-11-14 01:51:54 -0300

    Statting the credit card entity.

---
+++ src/main/java/com/mcp/my_wallet/model/CreditCard.java
@@ -0,0 +1,15 @@
+package com.mcp.my_wallet.model;
+
+public class CreditCard {
+    
+
+    // id
+    // card number
+    // bin number
+    // enum brand
+    // boolean activated
+    // cardCreditLimit
+    // List of dependents
+    // account
+
+}
