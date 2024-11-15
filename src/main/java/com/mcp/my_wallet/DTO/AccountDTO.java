114cf2b3d2b46c890c2c21bc847b4c037312a5f8
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-11-15 00:41:24 -0300

    AccountDTO created.

---
+++ src/main/java/com/mcp/my_wallet/DTO/AccountDTO.java
@@ -0,0 +1,7 @@
+package com.mcp.my_wallet.DTO;
+
+import com.mcp.my_wallet.model.Client;
+
+public record AccountDTO(Long id, Client client, Long accountCreditLimit) {
+    
+}
--- src/main/java/com/mcp/my_wallet/service/AccountService.java
+++ src/main/java/com/mcp/my_wallet/service/AccountService.java
@@ -2,6 +2,7 @@ package com.mcp.my_wallet.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.mcp.my_wallet.DTO.AccountDTO;
 import com.mcp.my_wallet.DTO.ClientDTO;
 import com.mcp.my_wallet.model.Account;
 import com.mcp.my_wallet.model.Client;
@@ -17,9 +18,10 @@ public class AccountService {
         Account account = repository.findById(id).get();
         return createDTO(account);
     }
-    
+
     //findAllAccounts
+    
     //updateCreditLimit(Long nreCreditLimit)
-    // ACcountDTO
+    
     // createDTO methods
 }
